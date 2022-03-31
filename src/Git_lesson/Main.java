2abe46f6485a61ccd61ef195b103d3c2756ba5ea
Anuar <anuarabitay@gmail.com>
2022-03-31 22:28:48 +0600

    switch to upper case

--- src/Git_lesson/Main.java
+++ src/Git_lesson/Main.java
@@ -3,6 +3,6 @@ package Git_lesson;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello QA GURU");
     }
 }
